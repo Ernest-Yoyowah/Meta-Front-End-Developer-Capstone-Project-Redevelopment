ea92a22eab98215d98726a70859f48046df4608c
ernkeyz <exconzy26@gmail.com>
2023-08-20 19:29:40 +0000

    reserve a table

--- src/pages/reservation/ReservationForm.jsx
+++ src/pages/reservation/ReservationForm.jsx
@@ -4,11 +4,12 @@ import './Reservation.css';
 
 const ReservationForm = () => {
   return (
-    <form style={{ display: 'grid', maxWidth: '200px', gap: '20px' }}>
-      <label htmlFor="res-date">Choose date</label>
-      <input type="date" id="res-date" />
-      <label htmlFor="res-time">Choose time</label>
-      <select id="res-time">
+    <div className='Fmain'>
+    <form className='FM'>
+      <label htmlFor="res-date" className='fm'>Date</label>
+      <input type="date" id="res-date" className='fm'/>
+      <label htmlFor="res-time" className='fm'>Time</label>
+      <select id="res-time" className='fm'>
         <option>17:00</option>
         <option>18:00</option>
         <option>19:00</option>
@@ -16,15 +17,16 @@ const ReservationForm = () => {
         <option>21:00</option>
         <option>22:00</option>
       </select>
-      <label htmlFor="guests">Number of guests</label>
-      <input type="number" placeholder="1" min="1" max="10" id="guests" />
-      <label htmlFor="occasion">Occasion</label>
-      <select id="occasion">
+      <label htmlFor="guests" className='fm'>Number of guests</label>
+      <input type="number" placeholder="1" min="1" max="10" id="guests" className='fm'/>
+      <label htmlFor="occasion" className='fm'>Occasion</label>
+      <select id="occasion" className='fm'>
         <option>Birthday</option>
         <option>Anniversary</option>
       </select>
       <input type="submit" value="Make Your reservation" />
     </form>
+    </div>
   );
 };
 
