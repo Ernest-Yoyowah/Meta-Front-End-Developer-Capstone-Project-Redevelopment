60d1a2e95ff58665e9a019878e83f05d3b5404bf
ernkeyz <exconzy26@gmail.com>
2023-08-22 21:24:36 +0000

    table

--- src/container/underconstruction/Uderconstruction.jsx
+++ src/container/underconstruction/Uderconstruction.jsx
@@ -6,10 +6,10 @@ const Uderconstruction = () => {
   return (
     <div className='under-const'>
       <div className='line'></div>
-      <h1>This Page is Under Development</h1>
-      <p>
-        We're working hard to bring you awesome features. Please check back later!
-      </p>
+        <h1>This Page is Under Development</h1>
+          <p>
+            We're working hard to bring you awesome features. Please check back later!
+          </p>
     </div>
   )
 }
