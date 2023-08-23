ac2ab156f6df35316bdff4b84d8204719f670f81
ernkeyz <exconzy26@gmail.com>
2023-08-23 21:28:02 +0000

    about page

---
+++ src/container/adj/line.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './line.css';
+
+const line = () => {
+  return (
+    <div className='mline'>line</div>
+  )
+}
+
+export default line
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,13 +2,15 @@ import Header from "../../components/Header";
 import React from 'react'
 import Navbar from '../../components/navbar/nav';
 import Footer from "../../components/footer/Footer";
-import Uderconstruction from "../../container/underconstruction/Uderconstruction";
+import Line from "../../container/adj/line";
+import History from "../../components/history/History";
 
 const About = () => {
   return (
     <>
     <Navbar />
-    <Uderconstruction />
+    <Line />
+    <History />
     <Footer />
     </>
   )
