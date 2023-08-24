a6e4613235aac5f628d841348486c508c29b220a
ernkeyz <exconzy26@gmail.com>
2023-08-24 21:33:15 +0000

    menu

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from '../../components/navbar/nav';
 import Uderconstruction from '../../container/underconstruction/Uderconstruction';
+import Line from "../../container/adj/line";
+import Main from '../../components/main/Main';
 import Footer from '../../components/footer/Footer';
+import MenuC from './MenuC';
 
 const Menu = () => {
   return (
     <>
     <Navbar />
-    <Uderconstruction />
+    {/* <Line /> */}
+    <MenuC />
+    <Main />
     <Footer />
     </>
   )
---
+++ src/pages/menu/MenuC.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Menu.css'
+const MenuC = () => {
+  return (
+    <div className='menu-header'>
+        <h1>Our Menu</h1>
+    </div>
+  )
+}
+
+export default MenuC
