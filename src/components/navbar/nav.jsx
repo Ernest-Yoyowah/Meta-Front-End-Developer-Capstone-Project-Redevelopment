51638640f70551a604cb23ff75c869b8defb1c45
ernkeyz <exconzy26@gmail.com>
2023-08-26 20:36:31 +0000

    nav-mobile

--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -1,55 +1,53 @@
-import React, { useState}  from 'react';
-import { BrowserRouter, Routes, Route, NavLink} from 'react-router-dom';
-
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-import {Home, About, Menu, Notpage, Order, Reservation, Login} from '../../pages';
-import './nav.css'
 import logo from '../../assets/logo.png';
+import './nav.css';
 
-const nav = () => {
+const Nav = () => {
   const [isClicked, setIsClicked] = useState(false);
 
   const toggleMenu = () => {
-    setIsClicked(isClicked);
+    setIsClicked(!isClicked); // Toggle the value of isClicked
   };
 
   return (
     <nav className='app__nav'>
-        <div className='app__nav-logo'>
-            <a href='/'>
-             <img src={logo} alt='logo' style={{ width: "15rem" }} />
-            </a>
-        </div>
-        <div className='app__nav-left'>
-          <div className='app__nav-links'>
-            <NavLink to='/home' >Home</NavLink>
-            <NavLink to='/about' >about</NavLink>
-            <NavLink to='/menu' >Menu</NavLink>
-            <NavLink to='/reservation' >Reservation</NavLink>
-            <NavLink to='/order' >Order Online</NavLink>
-            <NavLink to='/login' >Login</NavLink>
-          </div>
-        </div>
+      <div className='app__nav-logo'>
+        <a href='/'>
+          <img src={logo} alt='logo' className='logoNav' />
+        </a>
+      </div>
 
-        <div className='nav-mobile' >
-          <div className='app__nav-mobile'>
-            <button className='app__nav-toggle' onClick={toggleMenu}>
-              <FontAwesomeIcon icon={faBars} className='hamburger-icon' />
-                <div className={`app__nav-links ${isClicked ? 'open' : ''}`}>
-                  <NavLink to='/home' >Home</NavLink>
-                  <NavLink to='/about' >about</NavLink>
-                  <NavLink to='/menu' >Menu</NavLink>
-                  <NavLink to='/reservation' >Reservation</NavLink>
-                  <NavLink to='/order' >Order Online</NavLink>
-                  <NavLink to='/login' >Login</NavLink>
-                </div>
-            </button>
+      <div className='app__nav-left'>
+        <div className='app__nav-links'>
+          <NavLink to='/home'>Home</NavLink>
+          <NavLink to='/about'>About</NavLink>
+          <NavLink to='/menu'>Menu</NavLink>
+          <NavLink to='/reservation'>Reservation</NavLink>
+          <NavLink to='/order'>Order Online</NavLink>
+          <NavLink to='/login'>Login</NavLink>
+        </div>
+      </div>
+
+      <div className='nav-mobile'>
+        <div className='app__nav-mobile'>
+          <button className='app__nav-toggle' onClick={toggleMenu}>
+            <FontAwesomeIcon icon={faBars} className='hamburger-icon' />
+          </button>
+          <div className={`app__nav-links ${isClicked ? 'open' : ''}`}>
+            <NavLink to='/home'>Home</NavLink>
+            <NavLink to='/about'>About</NavLink>
+            <NavLink to='/menu'>Menu</NavLink>
+            <NavLink to='/reservation'>Reservation</NavLink>
+            <NavLink to='/order'>Order Online</NavLink>
+            <NavLink to='/login'>Login</NavLink>
           </div>
         </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default nav
+export default Nav;
