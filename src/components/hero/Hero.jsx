4fb2bd633c4833509dc7fbc53a606f570d43459c
ernkeyz <exconzy26@gmail.com>
2023-08-27 20:26:09 +0000

    completed header-mobile

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -19,23 +19,23 @@ const Hero = () => {
 
   return (
     <div className='header-container'>
-      <div className='head-text'>
-        <div className="text-content">
-          <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-              <p>
-                We are a family-owned Mediterranean restaurant,
-                focused on traditional recipes served with a modern twist.
-              </p>
-              <div className='cta'>
-              <NavLink to='/reservation'>Reserve a table</NavLink>
-              </div>
-        </div>
+          <div className='head-text'>
+            <div className="text-content">
+              <h1>Little Lemon</h1>
+                <h2>Chicago</h2>
+                  <p>
+                    We are a family-owned Mediterranean restaurant,
+                    focused on traditional recipes served with a modern twist.
+                  </p>
+                <div className='cta'>
+                  <NavLink to='/reservation'>Reserve a table</NavLink>
+                </div>
+          </div>
       <div className="image-container">
-    <img className='food-logo' src={logoF} alt='logo'/>
-  </div>
-</div>
-</div>
+        <img className='food-logo' src={logoF} alt='logo'/>
+      </div>
+      </div>
+    </div>
 
   )
 }
